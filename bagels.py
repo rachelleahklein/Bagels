b8f77e7c2a4468716bcb15067587f39b993ee4f0
Rachel Klein <rachel.leah.klein@gmail.com>
2015-08-14 21:34:19 -0700

    Initial commit of Bagels game, UI, and test files.

---
+++ bagels.py
@@ -0,0 +1,258 @@
+__author__ = 'rachel'
+
+from random import randrange, choice
+
+
+def did_player_win(codewords, digits):
+    """
+    This method is shared by the Game and ComputerPlayer classes because it can check whether a human
+    or a computer player has won the game.
+    """
+
+    if len(codewords) == digits and set(codewords) == set(["fermi"]):
+        return True
+    else:
+        return False
+
+
+class Game(object):
+    def __init__(self, guesser="human", digits=3):
+        """
+        Each game stores the number of guesses as an attribute. You will be able to choose
+        whether the human player or the computer player will be the guesser and it will
+        change how the game is played.
+        """
+
+        self.digits = digits
+        self.number_of_guesses = 0
+        if guesser == "human":
+            self.chosen_number = self.create_num(digits)
+
+    def create_num(self, digits):
+        """
+        This method generates a random number of a specified number of digits (3 to start).
+        No digits can repeat, and the number cannot start with 0. The digits are stored as
+        a list so it's easier to iterate through them and compare them to human guesses.
+        """
+
+        final_number = []
+
+        first_digit = randrange(1, 10)
+        final_number.append(first_digit)
+
+        infinite_loop_prevention = 0
+
+        while len(final_number) < digits:
+            infinite_loop_prevention += 1
+            if infinite_loop_prevention > 10:
+                raise Exception("Infinite loop detected!")
+            new_digit = randrange(0, 10)
+            if new_digit not in final_number:
+                final_number.append(new_digit)
+
+        return final_number
+
+    def compare_guess(self, human_guess):
+        """
+        This method takes the human player's guess and compares the digits and indices
+        to those in the random number the computer chose. It returns the code words "Bagel,"
+        "Pico," and/or "Fermi" based on whether any digits are right or in the right place.
+        """
+
+        self.number_of_guesses += 1
+        codewords = []
+        digits_place = 0
+
+        while digits_place < self.digits:
+            if human_guess[digits_place] == self.chosen_number[digits_place]:
+                codewords.insert(0, "Fermi")
+            elif human_guess[digits_place] in self.chosen_number:
+                codewords.insert(0, "Pico")
+            digits_place += 1
+
+        if len(codewords) == 0:
+            codewords.insert(0, "Bagels")
+
+        return codewords
+
+
+class ComputerPlayer(object):
+    def __init__(self, digits):
+        self.digits = digits
+        self.number_of_guesses = 0
+        self.possible_numbers = []
+        self.possible_digit_combinations = []
+        self.last_guess = []
+
+        # This part makes a list of all the numbers the computer can possibly guess based on
+        # the number of digits there are in the number.
+
+        minimum = [1]
+        maximum = [1]
+
+        for x in range(digits - 1):
+            minimum.append(0)
+        minimum = ''.join(map(str, minimum))
+        minimum = int(minimum)
+
+        for x in range(digits):
+            maximum.append(0)
+        maximum = ''.join(map(str, maximum))
+        maximum = int(maximum)
+
+        self.possible_numbers = range(minimum, maximum)
+
+        # The possible numbers are then made into lists of digits so it's easier to loop through
+        # them and find the digits you have just guessed.
+
+        for x in self.possible_numbers:
+            x = (map(int, str(x)))
+            self.possible_digit_combinations.append(x)
+
+        # Now we need to eliminate any numbers (lists of digits at this point) that duplicate any
+        # digits.
+
+        temporary_list = []
+
+        for list_of_digits in self.possible_digit_combinations:
+            if len(set(list_of_digits)) == self.digits:
+                temporary_list.append(list_of_digits)
+
+        self.possible_digit_combinations = temporary_list
+
+    def computer_guess(self, codewords=None):
+        """
+        This method allows the computer to guess the number chosen by a human player. Right now the
+        algorithm is not as sophisticated as it could be but it is definitely a big improvement over
+        random guessing.
+        """
+
+        if did_player_win(codewords or [], self.digits) is True:
+            return
+
+        if codewords == ["bagels"]:
+            self.bagels()
+
+        elif codewords is not None:
+
+            number_of_picos = codewords.count("pico")
+            number_of_fermis = codewords.count("fermi")
+
+            if len(codewords) == self.digits and set(codewords) == set(["pico"]):
+                self.all_pico()
+
+            elif len(codewords) == self.digits:
+                self.all_pico()
+                self.fermi()
+
+            elif number_of_fermis > 1:
+                self.multiple_fermis(number_of_fermis)
+
+            elif number_of_picos > 1:
+                self.multiple_picos(number_of_picos)
+
+            elif "fermi" in codewords:
+                self.fermi()
+
+            elif "pico" in codewords:
+                self.pico()
+
+        current_guess = choice(self.possible_digit_combinations)
+
+        self.possible_digit_combinations.remove(current_guess)
+        self.last_guess = current_guess
+
+    def bagels(self):
+        """
+        If the human player responds "Bagels" to the computer player's guess, the computer player will
+        rule out any numbers that contain any of the digits in that guess.
+        """
+
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            for digit in self.last_guess:
+                if digit in number:
+                    break
+            else:
+                if number not in new_possibilities:
+                    new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
+
+    def pico(self):
+        """
+        If the human player's response contains the word "Pico," the computer player will rule out any
+        numbers that don't have at least one digit in common with their last guess.
+        """
+
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            for digit in self.last_guess:
+                if digit in number:
+                    if number not in new_possibilities:
+                        new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
+
+    def all_pico(self):
+        """
+        If the human player's response is to type "Pico" as many times as there are digits in the number
+        being guessed, the computer player will rule out anything that doesn't have all three of those
+        digits.
+        """
+
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            for digit in self.last_guess:
+                if digit not in number:
+                    break
+            else:
+                if number not in new_possibilities:
+                    new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
+
+    def multiple_picos(self, number_of_picos):
+        """
+        If the human player types in "Pico" multiple times but not as many times as there are digits,
+        the computer player will rule out any number that doesn't have that number of digits in common.
+        """
+
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            digits_in_common = 0
+            for digit in self.last_guess:
+                if digit in number:
+                    digits_in_common += 1
+            if digits_in_common == number_of_picos:
+                if number not in new_possibilities:
+                    new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
+
+    def fermi(self):
+        """
+        If the human player's response contains the word "Fermi," the computer player will rule out any
+        numbers that don't have at least one digit in common and in the same place as in their last guess.
+        """
+
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            for x in range(0, self.digits):
+                if self.last_guess[x] == number[x]:
+                    if number not in new_possibilities:
+                        new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
+
+    def multiple_fermis(self, number_of_fermis):
+        """
+        If the human player's response contains the word "Fermi" multiple times, the computer player will rule out
+        any numbers that don't have at least that number of digits in common and in the same place as in their
+        last guess.
+        """
+        new_possibilities = []
+        for number in self.possible_digit_combinations:
+            digits_in_common = 0
+            for x in range(0, self.digits):
+                if self.last_guess[x] == number[x]:
+                    digits_in_common += 1
+            if digits_in_common == number_of_fermis:
+                if number not in new_possibilities:
+                    new_possibilities.append(number)
+        self.possible_digit_combinations = new_possibilities
---
+++ bagels_ui.py
@@ -0,0 +1,168 @@
+__author__ = 'rachel'
+import bagels
+import sys
+from time import sleep
+
+def guess():
+    """
+    This method prompts the user to guess a new number. It then converts the guess from
+    a string to a list of digits that can be compared to the computer's chosen number.
+    """
+    try:
+        current_guess = int(raw_input("Okay, I'm thinking of my number! What do you think it is??? "))
+        list_of_digits = map(int, str(current_guess))
+        if len(list_of_digits) != new_game.digits:
+            print "Hey, you're not guessing the right number of digits. " \
+                  "Your guess should be {} digits long.".format(new_game.digits)
+        elif len(set(list_of_digits)) < len(list_of_digits):
+            print "Remember, there are no repeating digits in this number!"
+        else:
+            current_codewords = new_game.compare_guess(list_of_digits)
+            results_of_guess(current_codewords, new_game)
+    except ValueError:
+        print "That's not a real guess, silly!"
+
+
+def results_of_guess(codewords, game):
+    """
+    This method shows the human player keywords based on how the computer player has evaluated
+    their guess. It also shows how many guesses the human player has used so far.
+    """
+
+    # Before giving feedback on the latest guess, the computer checks to see if the human won.
+
+    lowercase_codewords = []
+    for word in codewords:
+        lowercase_codewords.append(word.lower())
+
+    if bagels.did_player_win(lowercase_codewords, new_game.digits):
+        you_win()
+        sys.exit()
+    else:
+
+        # Rather than display codewords in the order they're originally added to the list,
+        # we want to list any "Pico" responses first and then any "Fermi" responses. This is
+        # the traditional Bagels order and it makes it harder for the human player to guess.
+
+        codewords.sort(reverse=True)
+        print " ".join(codewords)
+
+        if new_game.number_of_guesses == 1:
+            print "So far, you've made 1 guess."
+        else:
+            print "So far, you've made {} guesses.".format(new_game.number_of_guesses)
+
+
+def you_win():
+    """
+    Provides feedback to user when they win the game.
+    """
+    print "You win! That was awesome! It took you {} guesses.".format(new_game.number_of_guesses)
+
+
+def print_directions():
+    """
+    The user can choose to get the rules of the game when they start.
+    """
+    print "Oh good! I love explaining things!"
+    sleep(2)
+    raw_input("One player chooses a random number and the other one has to guess it. >> ")
+    raw_input \
+        ("Let's say I'm the one choosing a number. No digits can repeat, and the first digit won't be 0. >> ")
+    raw_input("If no digits are right, I'll say \'Bagels.\' >> ")
+    raw_input("If you guess one digit right but it's in the wrong place, I'll say \'Pico\'. >> ")
+    raw_input("If that happens twice, I'll say it twice. But I won't tell you which ones I mean. >> ")
+    raw_input("If you get any digit right AND it's in the right place, I'll say \'Fermi\' "
+              "(could also happen twice). >> ")
+    raw_input("Then you use my wacky code to keep guessing until you get it right! >> ")
+
+
+def compare_computer_guess(computer_player):
+    """
+    Prompts the user for input on how well the computer did on its last guess.
+    """
+    computer_guess = ''.join(map(str, computer_player.last_guess))
+    print computer_guess
+    guess_response = raw_input \
+        ("Okay, how did I do? Remember, type \'Bagels\' if none of the digits are right,\n"
+         "\'Pico\' every time I got a digit right but it's in the wrong place,\n"
+         "and \'Fermi\' every time a digit is right AND in the right place. >> ").lower()
+    guess_response = guess_response.split()
+    return guess_response
+
+
+def computer_guessing_game():
+    """
+    The whole process of a computer player guessing a number is contained in this method.
+    """
+    try:
+        number_of_digits = int(raw_input("""Do you want me to guess a 3 digit number? 4? 5?
+                Less than 3 isn't fun. And you can't pick more than 9 digits. You just can't, okay? >> """))
+        new_game = bagels.Game("computer", number_of_digits)
+        computer_player = bagels.ComputerPlayer(number_of_digits)
+    except ValueError:
+        print "Okay, whatever, 3 digits is good."
+        new_game = bagels.Game("computer", 3)
+        computer_player = bagels.ComputerPlayer(3)
+    computer_player.computer_guess()
+    codewords = []
+
+    while bagels.did_player_win(codewords, new_game.digits) is False:
+        computer_player.number_of_guesses += 1
+        codewords = compare_computer_guess(computer_player)
+
+        # Making sure the computer can read user codewords if they use commas.
+
+        codewords_sans_commas = []
+        for word in codewords:
+            if "," in word:
+                word = word.replace(',', '')
+                codewords_sans_commas.append(word)
+            else:
+                codewords_sans_commas.append(word)
+        codewords = codewords_sans_commas
+        try:
+            computer_player.computer_guess(codewords)
+        except IndexError:
+            raw_input("Hey, you either forgot your number or you're cheating. That's impossible! >> ")
+            print "Come back when you're ready to play for real."
+            sys.exit()
+    print "Yay! I won! And it only took me {} guesses.".format(computer_player.number_of_guesses)
+
+# A new game is created when this file is run from the console.
+
+if __name__ == '__main__':
+    raw_input(
+        "Hi! It's your computer! Care for some Bagels? And by \'Bagels\' I of course mean the fun logic game. >> ")
+    need_to_learn = raw_input \
+        ("Actually I don't care whether you do or not. :D Do you need to learn the rules? y/n??? >> ")
+    if need_to_learn.lower() == "y" or need_to_learn.lower() == "yes":
+        print_directions()
+    raw_input("Okay then, press enter if you're ready! I am so ready!!! >> ")
+    game_type = raw_input \
+        ("Please type 1 if you want me to pick the number for you to guess and 2 if you want to pick one for me. >> ")
+    if game_type == "1":
+        try:
+            number_of_digits = int(raw_input("""Do you want to guess a 3 digit number? 4? 5?
+                The more digits you pick, the harder it is to guess. Less than 3 isn't fun.
+                And you can't pick more than 9 digits. You just can't, okay? >> """))
+            new_game = bagels.Game("human", number_of_digits)
+        except ValueError:
+            print "Okay, whatever, 3 digits is good."
+            new_game = bagels.Game("human", 3)
+        while True:
+            guess()
+    elif game_type == "2":
+        computer_guessing_game()
+    else:
+        raw_input("Okay, so you're bad at decisions. I knew that. You can guess this time. >> ")
+        try:
+            number_of_digits = int(raw_input("""Do you want to guess a 3 digit number? 4? 5?
+                The more digits you pick, the harder it is to guess. Less than 3 isn't fun.
+                And you can't pick more than 9 digits. You just can't, okay? >> """))
+            new_game = bagels.Game("human", number_of_digits)
+        except ValueError:
+            print "Okay, whatever, 3 digits is good."
+            new_game = bagels.Game("human", 3)
+        while True:
+            guess()
